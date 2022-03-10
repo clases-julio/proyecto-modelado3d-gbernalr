62a2355df443ea748363f43dbd457645b7a7d1fa
gbernal <g.bernalr.2019@alumnos.urjc.es>
2022-03-10 10:48:36 +0100

    He añadido un par de funciones y constantes

---
+++ sparky.py
@@ -0,0 +1,92 @@
+import bpy
+
+'''*********************************************************************'''
+'''Funciones comunes útiles para selección/activación/borrado de objetos'''
+'''*********************************************************************'''
+def seleccionarObjeto(nombreObjeto): # Seleccionar un objeto por su nombre
+    bpy.ops.object.select_all(action='DESELECT') # deseleccionamos todos...
+    bpy.data.objects[nombreObjeto].select = True # ...excepto el buscado
+
+def activarObjeto(nombreObjeto): # Activar un objeto por su nombre
+    bpy.context.scene.objects.active = bpy.data.objects[nombreObjeto]
+
+def borrarObjeto(nombreObjeto): # Borrar un objeto por su nombre
+    seleccionarObjeto(nombreObjeto)
+    bpy.ops.object.delete(use_global=False)
+
+def borrarObjetos(): # Borrar todos los objetos
+    if(len(bpy.data.objects) != 0):
+        bpy.ops.object.select_all(action='SELECT')
+        bpy.ops.object.delete(use_global=False)
+
+'''****************************************************************'''
+'''Clase para realizar transformaciones sobre objetos seleccionados'''
+'''****************************************************************'''
+class Seleccionado:
+    def mover(v):
+        bpy.ops.transform.translate(
+            value=v, constraint_axis=(True, True, True))
+
+    def escalar(v):
+        bpy.ops.transform.resize(value=v, constraint_axis=(True, True, True))
+
+    def rotarX(v):
+        bpy.ops.transform.rotate(value=v, orient_axis='X')
+
+    def rotarY(v):
+        bpy.ops.transform.rotate(value=v, orient_axis='Y')
+
+    def rotarZ(v):
+        bpy.ops.transform.rotate(value=v, orient_axis='Z')
+
+'''**********************************************************'''
+'''Clase para realizar transformaciones sobre objetos activos'''
+'''**********************************************************'''
+class Activo:
+    def posicionar(v):
+        bpy.context.object.location = v
+
+    def escalar(v):
+        bpy.context.object.scale = v
+
+    def rotar(v):
+        bpy.context.object.rotation_euler = v
+
+    def renombrar(nombreObjeto):
+        bpy.context.object.name = nombreObjeto
+
+'''**************************************************************'''
+'''Clase para realizar transformaciones sobre objetos específicos'''
+'''**************************************************************'''
+class Especifico:
+    def escalar(nombreObjeto, v):
+        bpy.data.objects[nombreObjeto].scale = v
+
+    def posicionar(nombreObjeto, v):
+        bpy.data.objects[nombreObjeto].location = v
+
+    def rotar(nombreObjeto, v):
+        bpy.data.objects[nombreObjeto].rotation_euler = v
+
+'''************************'''
+'''Clase para crear objetos'''
+'''************************'''
+class Objeto:
+    def crearCubo(objName):
+        bpy.ops.mesh.primitive_cube_add(size=0.5, location=(0, 0, 0))
+        Activo.renombrar(objName)
+
+    def crearEsfera(objName):
+        bpy.ops.mesh.primitive_uv_sphere_add(radius=0.5, location=(0, 0, 0))
+        Activo.renombrar(objName)
+
+    def crearCono(objName):
+        bpy.ops.mesh.primitive_cone_add(radius1=0.5, location=(0, 0, 0))
+        Activo.renombrar(objName)
+
+'''************'''
+''' M  A  I  N '''
+'''************'''
+if __name__ == "__main__":
+    
+
